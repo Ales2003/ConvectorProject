a9ff3029df5332b5bcd06c636f47ca6c4bf240bf
admin <admin@DESKTOP-4D7AQF3>
2016-09-23 00:11:45 +0300

    version 1.0 Collection with GUI

--- src/ru/mail/ales_2003/convector/gui/ConvectorModel.java
+++ src/ru/mail/ales_2003/convector/gui/ConvectorModel.java
@@ -1,6 +1,8 @@
 
 package ru.mail.ales_2003.convector.gui;
 
+import static java.lang.Integer.valueOf;
+import static java.lang.Long.valueOf;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import ru.mail.ales_2003.convector.entity.Convector;
@@ -50,17 +52,17 @@ public class ConvectorModel extends AbstractTableModel
         // В зависимости от номера колонки возвращаем то или иное поле конвектора
         switch (col) {
             case 0:
-                return convector.getConvectorId();
+                return Long.toString(convector.getConvectorId());
             case 1:
-                return convector.getLength();
+                return Integer.toString(convector.getLength());
             case 2:
-                return convector.getWidth();
+                return Integer.toString(convector.getWidth());
             case 3:
-                return convector.getDepth();
+                return Integer.toString(convector.getDepth());
             case 4:
-                return convector.getPrice();
+                return Integer.toString(convector.getPrice());
             default:
-                return convector.getPower();
+                return Integer.toString(convector.getPower());
         }
     }
 }
