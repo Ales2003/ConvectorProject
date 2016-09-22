8372dcb8810c15deb01c3d8f8c87726baa514cc6
admin <admin@DESKTOP-4D7AQF3>
2016-09-22 23:44:37 +0300

    version 1.0 Collection with GUI

--- src/ru/mail/ales_2003/convector/gui/ConvectorModel.java
+++ src/ru/mail/ales_2003/convector/gui/ConvectorModel.java
@@ -1,10 +1,66 @@
 
 package ru.mail.ales_2003.convector.gui;
 
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import ru.mail.ales_2003.convector.entity.Convector;
+
 /**
  *
  * Model
  */
-public class ConvectorModel {
+public class ConvectorModel extends AbstractTableModel
+{
+    // Список загловков для колонок в таблице
+    private static final String[] headers = {"ID", "Длина", "Ширина", "Глубина", "Цена", "Мощность"};
     
+    // Здесь мы храним список конвекторов, которые будем отображать в таблице
+    private final List<Convector> convectors;
+
+    public ConvectorModel(List<Convector> convectors) {
+        this.convectors = convectors;
+    }
+    
+    @Override
+    // Получить количество строк в таблице - у нас это размер коллекции
+    public int getRowCount() {
+        return convectors.size();
+    }
+
+    @Override
+    // Получить количество столбцов - их у нас стольк же, сколько полей
+    // у класса Convector - всего шесть, можно сослаться на длину массива headers
+    public int getColumnCount() {
+        //return 6;
+        return headers.length;
+        
+    }
+
+    @Override
+    // Вернуть загловок колонки - мы его берем из массива headers
+    public String getColumnName(int col) {
+        return headers[col];
+    }
+    
+    @Override
+    // Получить объект для тображения в кокнретной ячейке таблицы
+    // В данном случае мы отдаем строковое представление поля
+    public Object getValueAt(int row, int col) {
+        Convector convector = convectors.get(row);
+        // В зависимости от номера колонки возвращаем то или иное поле конвектора
+        switch (col) {
+            case 0:
+                return convector.getConvectorId();
+            case 1:
+                return convector.getLength();
+            case 2:
+                return convector.getWidth();
+            case 3:
+                return convector.getDepth();
+            case 4:
+                return convector.getPrice();
+            default:
+                return convector.getPower();
+        }
+    }
 }
